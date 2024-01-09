200a2d4360a31c852e8806b30dd3646f77430f49
arvian75 <125346265+arvian75@users.noreply.github.com>
2024-01-09 21:18:39 +0700

    Update secrets.h

--- src/secrets.h
+++ src/secrets.h
@@ -1,10 +1,10 @@
-#define WIFI_SSID "yogsyogs"
-#define WIFI_PASSWORD "QWERTYUI"
-#define FIREBASE_HOST "https://triot-d7e6e-default-rtdb.firebaseio.com/"
-#define FIREBASE_AUTH "p6MtqXkstPUvFZLr61JAjAD4DAMUD97Spp4kE3PC"
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+#define FIREBASE_HOST ""
+#define FIREBASE_AUTH ""
 
 // #define USER_EMAIL "esp32cam@gmail.com"
 // #define USER_PASSWORD "trIOT123"
 
 // Insert Firebase storage bucket ID e.g bucket-name.appspot.com
-#define STORAGE_BUCKET_ID "triot-d7e6e.appspot.com"
+#define STORAGE_BUCKET_ID ""
