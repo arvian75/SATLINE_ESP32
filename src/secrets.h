dfe827da157664882fac46e944d6193d3846a79a
arvian75 <125346265+arvian75@users.noreply.github.com>
2024-01-09 21:18:59 +0700

    Update secrets.h

--- src/secrets.h
+++ src/secrets.h
@@ -3,8 +3,6 @@
 #define FIREBASE_HOST ""
 #define FIREBASE_AUTH ""
 
-// #define USER_EMAIL "esp32cam@gmail.com"
-// #define USER_PASSWORD "trIOT123"
 
 // Insert Firebase storage bucket ID e.g bucket-name.appspot.com
 #define STORAGE_BUCKET_ID ""
